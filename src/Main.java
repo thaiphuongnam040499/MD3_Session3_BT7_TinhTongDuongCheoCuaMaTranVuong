346c1302c688375aa63ee7af158985cabd02868f
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-08 20:19:32 +0700

    bt7

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i][i];
+        }
+        System.out.println("Tổng của đường chéo là: " + sum);
+    }
+}
